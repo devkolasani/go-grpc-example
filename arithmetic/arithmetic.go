7813b551c676f4462b63690871f5f3186ed9037e
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2022-12-11 18:38:26 +0530

    feat(arithmetic): function Operation implemented

---
+++ arithmetic/arithmetic.go
@@ -0,0 +1,19 @@
+package arithmetic
+
+import (
+	"golang.org/x/net/context"
+)
+
+type Server struct {
+}
+
+func (s *Server) Operation(ctx context.Context, req *OperationRequest) (*OperationResponse, error) {
+	var operationResult int64 = 0
+	switch req.OperationType {
+	case OperationType_ADD:
+		operationResult = req.OperandOne + req.OperandTwo
+	case OperationType_SUB:
+		operationResult = req.OperandOne - req.OperandTwo
+	}
+	return &OperationResponse{Result: operationResult}, nil
+}
